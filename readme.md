a569c50a543c145b8fd963c612f333729fc51b9f
gege <valasg@gmail.com>
2021-11-06 17:08:41 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+## ***Project description***
+* simple snake game
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { Game } from './classes/Game'
+
+window.addEventListener('load', () => {
+    const game = new Game()
+    game.start()
+})
